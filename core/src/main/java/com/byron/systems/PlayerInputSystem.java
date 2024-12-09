b87db561433107a3fa7dfbde6396cc249856d14c
Groxar <lrdolucho@gmail.com>
2024-12-08 23:51:12 -0400

    Movement is the same across different device powerfulness

--- core/src/main/java/com/byron/systems/PlayerInputSystem.java
+++ core/src/main/java/com/byron/systems/PlayerInputSystem.java
@@ -1,13 +1,13 @@
 package com.byron.systems;
 
+import static com.byron.constants.GeneralConstants.PLAYER_SPEED;
+
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
 import com.byron.components.BodyComponent;
-import com.byron.components.PositionComponent;
 import com.byron.components.StatusComponent;
 import com.byron.components.VelocityComponent;
 import com.byron.components.player.KeyboardComponent;
@@ -15,16 +15,17 @@ import com.byron.engine.GameResources;
 import com.byron.interfaces.IPlayerInputManager;
 import com.byron.models.player.PlayerAction;
 import com.byron.models.status.Action;
-import com.byron.utils.Mappers;
 import com.byron.models.status.Direction;
+import com.byron.utils.Mappers;
 
 import java.util.Stack;
 
 public class PlayerInputSystem extends IteratingSystem {
-    private ComponentMapper<StatusComponent> sm = Mappers.status;
-    private ComponentMapper<BodyComponent> bm = Mappers.body;
-    private ComponentMapper<VelocityComponent> vm = Mappers.velocity;
-    private IPlayerInputManager playerInputManager;
+
+    private final ComponentMapper<StatusComponent> sm = Mappers.status;
+    private final ComponentMapper<BodyComponent> bm = Mappers.body;
+    private final ComponentMapper<VelocityComponent> vm = Mappers.velocity;
+    private final IPlayerInputManager playerInputManager;
     OrthographicCamera camera;
 
     public PlayerInputSystem(IPlayerInputManager playerInputManager) {
@@ -47,19 +48,19 @@ public class PlayerInputSystem extends IteratingSystem {
 
             if (movementKeysPressed.contains(Direction.UP)) {
                 status.setDirection(Direction.UP);
-                bodyComponent.body.position.y += 0.05f;
+                bodyComponent.body.position.y += PLAYER_SPEED * deltaTime;
             }
             if (movementKeysPressed.contains(Direction.DOWN)) {
                 status.setDirection(Direction.DOWN);
-                bodyComponent.body.position.y -= 0.05f;
+                bodyComponent.body.position.y -= PLAYER_SPEED * deltaTime;
             }
             if (movementKeysPressed.contains(Direction.LEFT)) {
                 status.setDirection(Direction.LEFT);
-                bodyComponent.body.position.x -= 0.05f;
+                bodyComponent.body.position.x -= PLAYER_SPEED * deltaTime;
             }
             if (movementKeysPressed.contains(Direction.RIGHT)) {
                 status.setDirection(Direction.RIGHT);
-                bodyComponent.body.position.x += 0.05f;
+                bodyComponent.body.position.x += PLAYER_SPEED * deltaTime;
             }
         } else {
             status.setAction(Action.STANDING);
