474935ba29c216b2ff06d102c01033beedc71e93
Johnny <frank.grimes.inc@protonmail.com>
2024-12-15 15:42:52 -0500

    player gets weapon upon levelup to level 1

--- core/src/main/java/com/byron/systems/hud/ScoreChangeSystem.java
+++ core/src/main/java/com/byron/systems/hud/ScoreChangeSystem.java
@@ -2,24 +2,40 @@ package com.byron.systems.hud;
 
 import static com.byron.constants.MilestoneType.MAJOR;
 import static com.byron.constants.MilestoneType.MINOR;
+import static com.byron.constants.Weapon.REGULAR_SWORD;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.byron.components.WeaponComponent;
 import com.byron.components.events.MilestoneEvent;
 import com.byron.components.hud.ScoreEvent;
 import com.byron.components.hud.ScoreWidgetComponent;
 import com.byron.components.hud.TextComponent;
+import com.byron.components.sprite.SpriteComponent;
+import com.byron.components.weapons.DamageComponent;
+import com.byron.components.weapons.WeaponTag;
+import com.byron.factories.SpriteFactory;
+import com.byron.interfaces.IAgentService;
 import com.byron.utils.Mappers;
 
 public class ScoreChangeSystem extends IteratingSystem {
 
     private ImmutableArray<Entity> scoreWidgets;
+    IAgentService agentService;
+    private int level;
+    private int score;
+    private int pointsToNextLevel;
 
-    public ScoreChangeSystem() {
+    public ScoreChangeSystem(IAgentService agentService) {
         super(Family.all(ScoreEvent.class).get());
+        this.agentService = agentService;
+        level = 0;
+        score = 0;
+        pointsToNextLevel = 1000;
     }
 
     @Override
@@ -38,6 +54,7 @@ public class ScoreChangeSystem extends IteratingSystem {
         int scoreChange = Mappers.scoreEvent.get(entity).scoreChange;
         TextComponent scoreText = Mappers.text.get(scoreWidgets.first());
         int score = Math.max(Integer.parseInt(scoreText.text) + scoreChange, 0);
+        this.score = score;
         scoreText.text = String.valueOf(score);
         getEngine().removeEntity(entity);
         return score;
@@ -46,8 +63,20 @@ public class ScoreChangeSystem extends IteratingSystem {
     private void checkMilestones(int score) {
         if (score % 10000 == 0) {
             getEngine().addEntity(new Entity().add(new MilestoneEvent(MAJOR)));
-        } else if (score % 1000 == 0) {
+        } else if (score >= pointsToNextLevel) {
+            level++;
+            pointsToNextLevel = 1000;
             getEngine().addEntity(new Entity().add(new MilestoneEvent(MINOR)));
+            Sprite weaponSprite = SpriteFactory.getSprite(REGULAR_SWORD.name);
+            weaponSprite.setOriginCenter();
+            Entity weapon = new Entity()
+                .add(new WeaponTag())
+                .add(new SpriteComponent(weaponSprite))
+                .add(new DamageComponent(REGULAR_SWORD.damage));
+            getEngine().addEntity(weapon);
+
+            Entity player = agentService.getPlayer();
+            player.add(new WeaponComponent(weapon));
         }
     }
 }
