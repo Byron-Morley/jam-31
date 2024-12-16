d34a90891b5c6620437ca5e9842f9cef711e6b46
Groxar <lrdolucho@gmail.com>
2024-12-16 18:32:18 -0400

    Misc

--- core/src/main/java/com/byron/systems/PlayerInputSystem.java
+++ core/src/main/java/com/byron/systems/PlayerInputSystem.java
@@ -8,14 +8,12 @@ import static com.badlogic.gdx.graphics.Color.CLEAR;
 import static com.badlogic.gdx.graphics.Color.RED;
 import static com.byron.constants.Weapon.REGULAR_SWORD;
 import static com.byron.models.status.Direction.UP;
-import static com.byron.utils.Messages.PLAY_SOUND;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ai.msg.MessageManager;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
